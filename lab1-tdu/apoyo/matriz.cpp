01925a619d1670fd3b320ba4e6b85550c6f48730
J. Daniel Garcia <josedaniel.garcia@uc3m.es>
2020-09-24 23:06:34 +0200

    Lab1

---
+++ lab1-tdu/apoyo/matriz.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jdgarcia on 24/9/20.
+//
+
+#include "matriz.hpp"
---
+++ lab1-tdu/apoyo/matriz.hpp
@@ -0,0 +1,17 @@
+#ifndef APOYO_MATRIZ_HPP
+#define APOYO_MATRIZ_HPP
+
+namespace linalg {
+
+  class matriz {
+  public:
+    // Operaciones
+    //...
+  private:
+    int filas_;
+    int columnas_;
+    double * buffer_;
+  };
+}
+
+#endif //APOYO_MATRIZ_HPP
